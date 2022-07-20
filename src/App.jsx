f13ed744668c8d054fe488982aabad5f8f12cdb6
Alan Stall <alan_kstall@hotmail.com>
2022-07-20 19:34:13 -0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,16 @@ import './app.css'
 function App() {
   const [color, setColor] = useState("");
 
-  function changeColor(){
-    var hexColorText = "#"+color;
-    var hexColorArea = "#"+color;
-    var thisColorText = document.querySelector("h1");
-    var thisColorArea = document.querySelector("textarea")
-    thisColorText.style.color = hexColorText;
-    thisColorArea.style.backgroundColor = hexColorArea;
-  }
-
   return (
     <>
       <h4 id="title">Chose a color in hexadecimal</h4>
       <br></br>      
-      <input         
-        className="color-hexadecimal"
+      <input                 
         onChange={(e) => setColor(e.target.value)}
-        onSelect={changeColor}
       >        
       </input>
-      <h1 
-        className="this-color"        
-        >
-        This color!!!
-      </h1>
-      <textarea className='area-color'></textarea>
+      <h1 style={{color: `#${color}`}}>This color!!!</h1>      
+      <textarea style={{backgroundColor: `#${color}`}}></textarea>
     </>
   )
 }
